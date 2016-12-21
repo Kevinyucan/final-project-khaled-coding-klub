4e405c35e34bdcf34189c83fbed115f1ce289837
hadik9595 <hadik9595@HRH-IC0027320.wrdsb.ca>
2016-12-21 12:42:06 -0500

    initial

---
+++ html/src/com/pls/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.pls.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.pls.game.TeacherDefence;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TeacherDefence();
+        }
+}
